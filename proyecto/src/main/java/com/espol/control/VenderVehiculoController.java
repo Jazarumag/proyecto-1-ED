f8532bcc10a5f1713515a7ecaf0f8a07fad7cf22
Jesus Suarez <jedasuar@espol.edu.ec>
2024-06-03 12:32:23 -0500

    VenderVehiculo update botones

--- proyecto/src/main/java/com/espol/control/VenderVehiculoController.java
+++ proyecto/src/main/java/com/espol/control/VenderVehiculoController.java
@@ -5,9 +5,21 @@
 package com.espol.control;
 
 import com.espol.modelo.User;
+import com.espol.modelo.Vehiculo;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -18,6 +30,34 @@ public class VenderVehiculoController implements Initializable {
     
     private User usuario;
     
+    @FXML
+    private TextField precio;
+    
+    @FXML
+    private TextField km;
+    
+    @FXML
+    private TextField peso;
+    
+    @FXML
+    private TextField marca;
+    
+    @FXML
+    private TextField modelo;
+    
+    @FXML
+    private TextField anio;
+    
+    @FXML
+    private TextField motor;
+    
+    @FXML
+    private TextField transmision;
+    
+    @FXML
+    private TextField ubicacion;
+    
+    
     public void setUsuario(User usuario){
         this.usuario=usuario;
     }
@@ -27,6 +67,36 @@ public class VenderVehiculoController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    private void Vender(ActionEvent event) throws IOException{
+        int precio=Integer.parseInt(this.precio.getText());
+        int km=Integer.parseInt(this.km.getText());
+        int peso=Integer.parseInt(this.peso.getText());
+        String marca=this.marca.getText();
+        String modelo=this.modelo.getText();
+        String anio=this.anio.getText();
+        String motor=this.motor.getText();
+        String transmision=this.transmision.getText();
+        String ubicacion=this.ubicacion.getText();
+        
+        Vehiculo carro= new Vehiculo(precio, km, ubicacion);
+        
+    }
     
+    @FXML
+    private void Atras(ActionEvent event) throws IOException {
+        Alert alertaLogout=new Alert(Alert.AlertType.CONFIRMATION,"¿Quiere retroceder?");
+        if(alertaLogout.showAndWait().get()==ButtonType.OK){
+            cambiarPantallaLogin(event);
+        }
+    }
+    private void cambiarPantallaLogin(ActionEvent event) throws IOException{
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/com/espol/proyecto/primary.fxml"));
+        Parent root = (Parent) loader.load();
+        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene escena=new Scene(root);
+        stage.setScene(escena);
+        stage.show();
+    }
 }
